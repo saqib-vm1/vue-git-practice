a2121c4aeca4eb022838ec37eded94f7525e1ae5
Mohd Saqib <mohd.saqib@vidyamantra.com>
2023-01-23 17:03:59 +0530

    v-model two way input binding practice

---
+++ src/components/v-model/VModel.vue
@@ -0,0 +1,21 @@
+<template>
+    <h1>v-model binding</h1>
+    <input v-model="firstName" />
+    <CustomInput v-model="lastName" />
+    <VModelArguments v-model:age="age" v-model:address="address" />
+    <ModelModifiers v-model:title.upperCase="title" />
+    <p>{{ firstName }} - {{ lastName }} - {{ age }} - {{ address }} - upper cased : {{ title }}</p>
+</template>
+
+<script setup>
+    import { ref } from 'vue';
+    import CustomInput from './CustomInput.vue';
+    import VModelArguments from './VModelArguments.vue';
+    import ModelModifiers from './ModelModifiers.vue';
+
+    const firstName = ref('default');
+    const lastName = ref('');
+    const age = ref('');
+    const address = ref('');
+    const title = ref('');
+</script>
---
+++ src/components/v-model/VModelArguments.vue
@@ -0,0 +1,12 @@
+<template>
+    <input :value="age" @input="$emit('update:age', $event.target.value)" />
+    <input :value="address" @input="$emit('update:address', $event.target.value)" />
+</template>
+
+<script setup>
+    import { defineProps, defineEmits } from 'vue';
+
+    defineProps(['age', 'address']);
+    defineEmits(['update:age', 'update:address']);
+
+</script>
