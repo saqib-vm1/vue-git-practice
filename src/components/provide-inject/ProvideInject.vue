517b3dbe9d9beb404877591769b3537f4164d08c
Mohd Saqib <mohd.saqib@vidyamantra.com>
2023-01-23 17:08:28 +0530

    passing data from parent to child using provide/inject methods

---
+++ src/components/provide-inject/ProvideInject.vue
@@ -0,0 +1,16 @@
+<template>
+    <h1>Provider Component A - {{ count }}</h1>
+    <ProvideInjectB />
+</template>
+
+<script setup>
+    import { provide, ref, readonly } from 'vue';
+    import ProvideInjectB from './ProvideInjectB.vue'
+
+    let count = ref(0);
+    function updateCount() {
+        count.value++
+    }
+    provide('count', readonly(count));
+    provide('updateCount', updateCount)
+</script>
---
+++ src/components/provide-inject/ProvideInjectC.vue
@@ -0,0 +1,12 @@
+<template>
+    <h1>Consumer Component C</h1>
+    <p>{{ count }}</p>
+    <button @click="updateCount">UpdateCount</button>
+</template>
+
+<script setup>
+    import { inject } from 'vue';
+
+    let count = inject('count');
+    let updateCount = inject('updateCount');
+</script>
