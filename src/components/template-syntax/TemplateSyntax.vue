49646f5b4593737b90256941667383290a29cc71
Mohd Saqib <mohd.saqib@vidyamantra.com>
2023-01-23 10:08:36 +0530

    text interpolation using mustache '{{}}' syntax

--- src/components/template-syntax/TemplateSyntax.vue
+++ src/components/template-syntax/TemplateSyntax.vue
@@ -1,3 +1,9 @@
 <template>
-    <h1>Child Component</h1>
+    <h1>Template Syntax Component</h1>
+    <hr />
+    <TextInterpolation />
 </template>
+
+<script setup>
+ import TextInterpolation from './TextInterpolation.vue';
+</script>
---
+++ src/components/template-syntax/TextInterpolation.vue
@@ -0,0 +1,11 @@
+<template>
+    <h1>Text Interpolation</h1>
+    <h4>The most basic form of data binding is text interpolation using the "Mustache" syntax (double curly braces):</h4>
+    <p>Reactive message with text interpolation : {{ reactiveMsg }}</p>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+    // the 'ref()' function provides the reactive data.
+    let reactiveMsg = ref('Reactive Message');
+</script>
